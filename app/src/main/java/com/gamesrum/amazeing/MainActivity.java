7123665576d25ac3853a1e1ff302a737002e9c2a
tmartinez <tmartinez@switchfly.com>
2015-01-28 23:28:47 -0700

    First Commit - base app

---
+++ app/src/main/java/com/gamesrum/amazeing/MainActivity.java
@@ -0,0 +1,40 @@
+package com.gamesrum.amazeing;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_main);
+
+        WebView daWebView = (WebView) findViewById(R.id.daWebView);
+        daWebView.setWebViewClient(new WebViewClient());
+
+        WebSettings settings = daWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        daWebView.loadUrl("http://android.amazeing.in/");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return false;
+    }
+}
